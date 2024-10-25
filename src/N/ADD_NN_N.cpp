d4f554174316bdb5a41170a78c5aea9f74098d84
rennelv <55945621+Rennelv@users.noreply.github.com>
2024-10-25 22:40:02 +0300

    use snake_case

--- src/N/ADD_NN_N.cpp
+++ src/N/ADD_NN_N.cpp
@@ -7,40 +7,40 @@
 #include "N/LongNatural.hpp"
 
 LongNatural ADD_NN_N(const LongNatural& a, const LongNatural& b) {
-    std::vector<uint8_t> aArr = a.getArr();  // получаем массивы цифр числа а
-    std::vector<uint8_t> bArr = b.getArr();  // получаем массивы цифр числа b
+    std::vector<uint8_t> a_arr = a.getArr();  // получаем массивы цифр числа а
+    std::vector<uint8_t> b_arr = b.getArr();  // получаем массивы цифр числа b
 
     // если число а меньше числа b, то меняем их местами
     if (COM_NN_D(a, b) == 1) {
-        aArr = b.getArr();
-        bArr = a.getArr();
+        a_arr = b.getArr();
+        b_arr = a.getArr();
     }
 
-    std::vector<uint8_t> resultArr;      // массив для результата
-    resultArr.reserve(aArr.size() + 1);  // резервируем место для результата
+    std::vector<uint8_t> result_arr;       // массив для результата
+    result_arr.reserve(a_arr.size() + 1);  // резервируем место для результата
 
     uint8_t carry = 0;  // перенос
-    size_t aLen = aArr.size();
-    size_t bLen = bArr.size();
+    size_t a_len = a_arr.size();
+    size_t b_len = b_arr.size();
 
-    for (size_t i = 0; i < aLen; i++) {
-        size_t aIdx = aLen - i - 1;        // индекс для числа а с конца
-        uint8_t sum = aArr[aIdx] + carry;  // складываем цифру числа а и перенос
+    for (size_t i = 0; i < a_len; i++) {
+        size_t a_idx = a_len - i - 1;        // индекс для числа а с конца
+        uint8_t sum = a_arr[a_idx] + carry;  // складываем цифру числа а и перенос
 
-        if (i < bLen) {
-            size_t bIdx = bLen - i - 1;  // индекс для числа b с конца
-            sum += bArr[bIdx];           // если есть соответсвующая цифра числа b, то прибавляем ее
+        if (i < b_len) {
+            size_t b_idx = b_len - i - 1;  // индекс для числа b с конца
+            sum += b_arr[b_idx];           // если есть соответсвующая цифра числа b, то прибавляем ее
         }
 
-        carry = sum / 10;               // вычисляем перенос
-        resultArr.push_back(sum % 10);  // добавляем остаток от деления на 10 в результат
+        carry = sum / 10;                // вычисляем перенос
+        result_arr.push_back(sum % 10);  // добавляем остаток от деления на 10 в результат
     }
 
     if (carry != 0) {
-        resultArr.push_back(carry);  // если есть перенос, то добавляем его в результат
+        result_arr.push_back(carry);  // если есть перенос, то добавляем его в результат
     }
 
-    std::reverse(resultArr.begin(), resultArr.end());  // переворачиваем массив
+    std::reverse(result_arr.begin(), result_arr.end());  // переворачиваем массив
 
-    return LongNatural(resultArr);  // возвращаем результат
+    return LongNatural(result_arr);  // возвращаем результат
 }
