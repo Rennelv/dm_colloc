6452541a880e358145580c51cde23da001d7397d
LordPumpk1n <gladilin20052@gmail.com>
2024-10-26 23:20:42 +0300

    SUB_NN_N TESTS

--- tests/N/test_SUB_NN_N.cpp
+++ tests/N/test_SUB_NN_N.cpp
@@ -2,12 +2,10 @@
 
 #include <vector>
 
-#include "N/SUB_NN_N.hpp"
 #include "N/LongNatural.hpp"
+#include "N/SUB_NN_N.hpp"
 
 TEST(test_SUB_NN_N, SameLength) {
-    //std::vector<uint8_t> arr1 = {4, 5, 4};
-    //std::vector<uint8_t> arr2 = {1, 2, 3};
     LongNatural a({3, 2, 1});
     LongNatural b({1, 1, 1});
     LongNatural res = SUB_NN_N(a, b);
