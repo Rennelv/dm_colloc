bf265c2d6126303d0282c07a5302c754986b7d23
kunakkova <kunakovamaria@mail.ru>
2024-10-27 17:49:33 +0300

    function MUL_Nk_N

--- src/N/MUL_Nk_N.cpp
+++ src/N/MUL_Nk_N.cpp
@@ -5,10 +5,9 @@ LongNatural MUL_Nk_N(const LongNatural& a, size_t k) {
     if (a.getLength() == 1 && a.at(0) == 0) {
         return LongNatural({0});
     }
-    
-    std::vector<uint8_t> a_arr = a.getArr(); // получаем массив цифр числа
-    a_arr.insert(a_arr.end(), k, 0); // добавляем k нулей в конец массива цифр числа, то есть умножаем на 10^k
-    
+
+    std::vector<uint8_t> a_arr = a.getArr();  // получаем массив цифр числа
+    a_arr.insert(a_arr.end(), k, 0);  // добавляем k нулей в конец массива цифр числа, то есть умножаем на 10^k
+
     return LongNatural(a_arr);
 }
-
--- tests/N/test_MUL_Nk_N.cpp
+++ tests/N/test_MUL_Nk_N.cpp
@@ -1,35 +1,34 @@
 #include <gtest/gtest.h>
 
-#include "N/MUL_Nk_N.hpp"
 #include "N/LongNatural.hpp"
+#include "N/MUL_Nk_N.hpp"
 
 TEST(test_MUL_Nk_N, MultiplyByZeroPower) {
-    LongNatural a1({1, 2, 3}); // 123
+    LongNatural a1({1, 2, 3});  // 123
     LongNatural result = MUL_Nk_N(a1, 0);
-    EXPECT_EQ(result.toString(), "123"); // 123 * 10^0 = 123
+    EXPECT_EQ(result.toString(), "123");  // 123 * 10^0 = 123
 }
 
 TEST(test_MUL_Nk_N, MultiplyByOnePower) {
-    LongNatural a2({1, 2, 3}); // 123
+    LongNatural a2({1, 2, 3});  // 123
     LongNatural result = MUL_Nk_N(a2, 1);
-    EXPECT_EQ(result.toString(), "1230"); // 123 * 10^1 = 1230
+    EXPECT_EQ(result.toString(), "1230");  // 123 * 10^1 = 1230
 }
 
 TEST(test_MUL_Nk_N, MultiplyByTwoPowers) {
-    LongNatural a3({1, 2, 3}); // 123
+    LongNatural a3({1, 2, 3});  // 123
     LongNatural result = MUL_Nk_N(a3, 2);
-    EXPECT_EQ(result.toString(), "12300"); // 123 * 10^2 = 12300
+    EXPECT_EQ(result.toString(), "12300");  // 123 * 10^2 = 12300
 }
 
 TEST(test_MUL_Nk_N, MultiplyByLargePower) {
-    LongNatural a4({1, 2, 3}); // 123
+    LongNatural a4({1, 2, 3});  // 123
     LongNatural result = MUL_Nk_N(a4, 5);
-    EXPECT_EQ(result.toString(), "12300000"); // 123 * 10^5 = 12300000
+    EXPECT_EQ(result.toString(), "12300000");  // 123 * 10^5 = 12300000
 }
 
 TEST(test_MUL_Nk_N, MultiplyZeroByPower) {
-    LongNatural a5({0}); // 0
+    LongNatural a5({0});  // 0
     LongNatural result = MUL_Nk_N(a5, 4);
-    EXPECT_EQ(result.toString(), "0"); // 0 * 10^4 = 0
+    EXPECT_EQ(result.toString(), "0");  // 0 * 10^4 = 0
 }
-
