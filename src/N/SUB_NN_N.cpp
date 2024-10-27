f19a3f203b63b5f1798fc3c236ba34f8bfdac2cf
LordPumpk1n <gladilin20052@gmail.com>
2024-10-27 12:15:44 +0300

    a

--- src/N/SUB_NN_N.cpp
+++ src/N/SUB_NN_N.cpp
@@ -19,16 +19,16 @@ LongNatural SUB_NN_N(const LongNatural& a, const LongNatural& b) {
     std::vector<uint8_t> result_arr;   // массив для результата
     result_arr.reserve(a_arr.size());  // резервируем место для результата
 
-    uint8_t carry = 0;         // перенос
+    uint8_t carry = 0;            // перенос
     size_t a_len = a_arr.size();  // длина числа a
     size_t b_len = b_arr.size();  // длина числа b
 
     for (size_t i = 0; i < b_len; i++) {
-        size_t a_idx = a_len - 1 - i;   // индес числа a
-        size_t b_idx = b_len - 1 - i;   // индекс числа b
-        if (a_arr[a_idx]>= b_arr[b_idx] + carry) {
+        size_t a_idx = a_len - 1 - i;  // индес числа a
+        size_t b_idx = b_len - 1 - i;  // индекс числа b
+        if (a_arr[a_idx] >= b_arr[b_idx] + carry) {
             result_arr.push_back(a_arr[a_idx] - carry - b_arr[b_idx]);  // из выбранной цифры вычитаем цифру второго числа и перенс
-            carry = 0;                                          // не занимаем из следующей цифры
+            carry = 0;                                                  // не занимаем из следующей цифры
         } else {
             result_arr.push_back(a_arr[a_idx] + 10 - carry - b_arr[b_idx]);  // занимаем у следующей цифры и вычитаем из выбранной цифру второго числа
             carry = 1;
@@ -36,7 +36,7 @@ LongNatural SUB_NN_N(const LongNatural& a, const LongNatural& b) {
     }
 
     for (size_t i = b_len; i < a_len; i++) {  // добавляем оставшиеся цифры чила a и продолжаем занимать, пока число не будет больше 0
-        size_t a_idx = a_len - 1 - i;   // индекс числа a
+        size_t a_idx = a_len - 1 - i;  // индекс числа a
         if (a_arr[a_idx] >= carry) {
             result_arr.push_back(a_arr[a_idx] - carry);
             carry = 0;
