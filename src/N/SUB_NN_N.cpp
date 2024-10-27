34fa2423a2d371893f56ce78a55e155195b84071
LordPumpk1n <gladilin20052@gmail.com>
2024-10-27 11:58:31 +0300

    fix super strange bugs

--- src/N/SUB_NN_N.cpp
+++ src/N/SUB_NN_N.cpp
@@ -20,25 +20,28 @@ LongNatural SUB_NN_N(const LongNatural& a, const LongNatural& b) {
     result_arr.reserve(a_arr.size());  // резервируем место для результата
 
     uint8_t carry = 0;         // перенос
-    int a_len = a_arr.size();  // длина числа a
-    int b_len = b_arr.size();  // длина числа b
+    size_t a_len = a_arr.size();  // длина числа a
+    size_t b_len = b_arr.size();  // длина числа b
 
-    for (int i = b_len - 1; i >= 0; i--) {
-        if (a_arr[i] - carry >= b_arr[i]) {
-            result_arr.push_back(a_arr[i] - carry - b_arr[i]);  // из выбранной цифры вычитаем цифру второго числа и перенс
+    for (size_t i = 0; i < b_len; i++) {
+        size_t a_idx = a_len - 1 - i;
+        size_t b_idx = b_len - 1 - i;
+        if (a_arr[a_idx]>= b_arr[b_idx] + carry) {
+            result_arr.push_back(a_arr[a_idx] - carry - b_arr[b_idx]);  // из выбранной цифры вычитаем цифру второго числа и перенс
             carry = 0;                                          // не занимаем из следующей цифры
         } else {
-            result_arr.push_back(a_arr[i] + 10 - carry - b_arr[i]);  // занимаем у следующей цифры и вычитаем из выбранной цифру второго числа
+            result_arr.push_back(a_arr[a_idx] + 10 - carry - b_arr[b_idx]);  // занимаем у следующей цифры и вычитаем из выбранной цифру второго числа
             carry = 1;
         }
     }
 
-    for (int i = a_len - 1; i >= b_len; i--) {  // добавляем оставшиеся цифры чила a и продолжаем занимать, пока число не будет больше 0
-        if (a_arr[i] >= carry) {
-            result_arr.push_back(a_arr[i] - carry);
+    for (size_t i = b_len; i < a_len; i++) {  // добавляем оставшиеся цифры чила a и продолжаем занимать, пока число не будет больше 0
+        size_t a_idx = a_len - 1 - i;
+        if (a_arr[a_idx] >= carry) {
+            result_arr.push_back(a_arr[a_idx] - carry);
             carry = 0;
         } else {
-            result_arr.push_back(a_arr[i] + 10 - carry);
+            result_arr.push_back(a_arr[a_idx] + 10 - carry);
             carry = 1;
         }
     }
--- tests/N/test_SUB_NN_N.cpp
+++ tests/N/test_SUB_NN_N.cpp
@@ -1,51 +1,50 @@
 #include <gtest/gtest.h>
 
-#include <vector>
-
 #include "N/LongNatural.hpp"
 #include "N/SUB_NN_N.hpp"
 
-TEST(test_SUB_NN_N, SameLength) {
-    LongNatural a({3, 2, 1});
-    LongNatural b({1, 1, 1});
-    LongNatural res = SUB_NN_N(a, b);
-    std::vector<uint8_t> arr3 = {2, 1, 0};
-    LongNatural correctRes(arr3);
-    EXPECT_EQ(res.getArr(), correctRes.getArr());
-}
-/*
-TEST(test_SUB_NN_N, FirstLower) {
-    LongNatural a({1, 2, 1});
-    LongNatural b({4, 4, 4});
-    LongNatural res = SUB_NN_N(a, b);
-    std::vector<uint8_t> arr3 = {3, 2, 3};
-    LongNatural correctRes(arr3);
-    EXPECT_EQ(res.getArr(), correctRes.getArr());
-}*/
-
-TEST(test_SUB_NN_N, DifferentLength) {
-    LongNatural b({1, 1, 4, 4, 4});
-    LongNatural a({1, 2, 1});
-    LongNatural res = SUB_NN_N(a, b);
-    std::vector<uint8_t> arr3 = {1, 1, 3, 2, 3};
-    LongNatural correctRes(arr3);
-    EXPECT_EQ(res.getArr(), correctRes.getArr());
+TEST(SUB_NN_N, SubtractEqualNumbers) {
+    LongNatural a({1, 2, 3, 4});
+    LongNatural b({1, 2, 3, 4});
+    LongNatural result = SUB_NN_N(a, b);
+    LongNatural expected({0});
+    EXPECT_EQ(result, expected);
 }
 
-TEST(test_SUB_NN_N, Carry) {
-    LongNatural a({2, 0, 0, 0, 0, 0});
-    LongNatural b({9});
-    LongNatural res = SUB_NN_N(a, b);
-    std::vector<uint8_t> arr3 = {1, 9, 9, 9, 9, 1};
-    LongNatural correctRes(arr3);
-    EXPECT_EQ(res.getArr(), correctRes.getArr());
+TEST(SUB_NN_N, SubtractSmallerFromLarger) {
+    LongNatural a({2, 3, 4, 5});
+    LongNatural b({1, 2, 3, 4});
+    LongNatural result = SUB_NN_N(a, b);
+    LongNatural expected({1, 1, 1, 1});
+    EXPECT_EQ(result, expected);
 }
 
-TEST(test_SUB_NN_N, InsignificantZero) {
-    LongNatural a({9, 9, 9, 9, 9});
-    LongNatural b({9, 9, 9, 9, 8});
-    LongNatural res = SUB_NN_N(a, b);
-    std::vector<uint8_t> arr3 = {2, 1, 0};
-    LongNatural correctRes(arr3);
-    EXPECT_EQ(res.getArr(), correctRes.getArr());
+TEST(SUB_NN_N, SubtractWithBorrowing) {
+    LongNatural a({1, 0, 0, 0});
+    LongNatural b({1});
+    LongNatural result = SUB_NN_N(a, b);
+    LongNatural expected({9, 9, 9});
+    EXPECT_EQ(result, expected);
+}
+
+TEST(SUB_NN_N, SubtractWithLeadingZeros) {
+    LongNatural a({1, 0, 0, 0});
+    LongNatural b({9, 9, 9});
+    LongNatural result = SUB_NN_N(a, b);
+    LongNatural expected({1});
+    EXPECT_EQ(result, expected);
+}
+
+TEST(SUB_NN_N, SubtractLargerFromSmallerThrows) {
+    LongNatural a({1, 2, 3});
+    LongNatural b({4, 5, 6});
+    EXPECT_THROW(SUB_NN_N(a, b), std::logic_error);
+}
+
+TEST(SUB_NN_N, SubtractWithDifferentLengths) {
+    LongNatural a({1, 0, 0, 0});
+    LongNatural b({1, 0});
+    LongNatural result = SUB_NN_N(a, b);
+    LongNatural expected({9, 9, 0});
+    EXPECT_EQ(result, expected);
 }
