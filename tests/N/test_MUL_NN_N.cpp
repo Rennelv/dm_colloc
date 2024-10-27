6406a2373aff4eb31f1d53e4faf60aac14724b6a
LordPumpk1n <gladilin20052@gmail.com>
2024-10-27 22:57:29 +0300

    fix test

--- tests/N/test_MUL_NN_N.cpp
+++ tests/N/test_MUL_NN_N.cpp
@@ -15,16 +15,15 @@ TEST(test_MUL_NN_N, OneLongOneShort) {
     LongNatural a({1, 1, 0, 2, 5});
     LongNatural b({2});
     LongNatural res = MUL_NN_N(a, b);
-    LongNatural expected({2, 2, 0, 5, 0}); // 11025 * 2 = 22050
+    LongNatural expected({2, 2, 0, 5, 0});  // 11025 * 2 = 22050
     EXPECT_EQ(res, expected);
 }
-
 
 TEST(test_MUL_NN_N, OneShortOneLong) {
     LongNatural a({2});
     LongNatural b({1, 1, 0, 2, 5});
     LongNatural res = MUL_NN_N(a, b);
-    LongNatural expected({2, 2, 0, 5, 0}); // 2 * 11025 = 22050
+    LongNatural expected({2, 2, 0, 5, 0});  // 2 * 11025 = 22050
     EXPECT_EQ(res, expected);
 }
 
@@ -32,7 +31,7 @@ TEST(test_MUL_NN_N, MulZero) {
     LongNatural a({0});
     LongNatural b({1, 1, 0, 2, 5});
     LongNatural res = MUL_NN_N(a, b);
-    LongNatural expected({0}); // 0 * 11025 = 0
+    LongNatural expected({0});  // 0 * 11025 = 0
     EXPECT_EQ(res, expected);
 }
 
@@ -40,6 +39,6 @@ TEST(test_MUL_NN_N, LongNumbers) {
     LongNatural a({2, 5, 0, 0});
     LongNatural b({2, 5, 0, 0});
     LongNatural res = MUL_NN_N(a, b);
-    LongNatural expected({6, 2, 5, 0, 0, 0, 0}); // 250 * 250 = 6250000
+    LongNatural expected({6, 2, 5, 0, 0, 0, 0});  // 250 * 250 = 6250000
     EXPECT_EQ(res, expected);
 }
