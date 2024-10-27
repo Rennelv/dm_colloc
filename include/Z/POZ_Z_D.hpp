9edc4da5e3d72ed1c37cebea08b1b629987b0406
33sasha81 <sasaderevagina87@gmail.com>
2024-10-27 19:55:43 +0300

    function POZ_Z_D

---
+++ include/Z/POZ_Z_D.hpp
@@ -0,0 +1,20 @@
+#ifndef POZ_Z_D_HPP
+#define POZ_Z_D_HPP
+
+#include <Z/LongInteger.hpp>
+
+/*
+    Выполнила Деревягина Александра 3381
+
+    Функция POZ_Z_D принимает число и узнает его знак.
+    Принимает:
+        a - целое число
+    Возвращает:
+        0 - если нуль
+        1 - если отрицательное
+        2 - если положительное
+*/
+
+int POZ_Z_D(const LongInteger& a);
+
+#endif  // POZ_Z_D_HPP
