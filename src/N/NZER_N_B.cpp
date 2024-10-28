9c839ab6ef7eabbe62c5a562108c24b3218d581d
kzkken <zhabiykzkken@mail.ru>
2024-10-28 13:47:52 +0300

    NZER_N_B

--- src/N/NZER_N_B.cpp
+++ src/N/NZER_N_B.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 bool NZER_N_B(const LongNatural &a)
 {
-    if (a.getLenght() == 1 && a.radix(0) == 0)
+    if (a.getLength() == 1 && a.radix(0) == 0)
     { // проверяем, что количество цифр 1 и эта цифра 0
         return true;
     }
