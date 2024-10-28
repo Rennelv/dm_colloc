cd2d9163db8e4903313fd2a776bf41396e34a16b
kzkken <zhabiykzkken@mail.ru>
2024-10-28 14:08:41 +0300

    NZER_N_B

--- tests/N/test_NZER_N_B.cpp
+++ tests/N/test_NZER_N_B.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+
 #include <vector>
 
 #include "../include/N/LongNatural.hpp"
@@ -6,9 +7,14 @@
 
 using namespace std;
 
-TEST(Test_NZER_N_B, N2) {
+TEST(Test_NZER_N_B, N2)
+{
     LongNatural a({0});
     LongNatural b({2, 0, 3});
+    LongNatural c({1, 0});
+    LongNatural d({5});
     EXPECT_EQ(NZER_N_B(a), true);
     EXPECT_EQ(NZER_N_B(b), false);
+    EXPECT_EQ(NZER_N_B(c), false);
+    EXPECT_EQ(NZER_N_B(d), false);
 }
