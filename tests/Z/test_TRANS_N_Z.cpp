cce20b31d4c75b02b16be60c173f4c75e3b52d4a
aassiya <aksreeva@mail.ru>
2024-10-28 23:59:49 +0300

    function corrected and new test added

--- tests/Z/test_TRANS_N_Z.cpp
+++ tests/Z/test_TRANS_N_Z.cpp
@@ -4,8 +4,12 @@
 #include "Z/LongInteger.hpp"
 #include "Z/TRANS_N_Z.hpp"
 
-TEST(test_TRANS_N_Z, PositiveNumber)
-{
-    LongNatural a({1, 2, 3});           // 123
-    EXPECT_EQ(TRANS_N_Z(a).toString(), "123"); // 123
+TEST(test_TRANS_N_Z, PositiveNumber1) {
+    LongNatural a({1, 2, 3});                   // 123
+    EXPECT_EQ(TRANS_N_Z(a).toString(), "123");  // 123
+}
+
+TEST(test_TRANS_N_Z, PositiveNumber2) {
+    LongNatural a({3, 2, 1});                   // 321
+    EXPECT_EQ(TRANS_N_Z(a).toString(), "321");  // 321
 }
