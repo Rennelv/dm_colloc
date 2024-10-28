caf0317a084d40944fd46e57433582720746bd48
aassiya <aksreeva@mail.ru>
2024-10-29 01:23:01 +0300

    zero test removed

--- tests/Q/test_INT_Q_B.cpp
+++ tests/Q/test_INT_Q_B.cpp
@@ -12,13 +12,6 @@ TEST(test_INT_Q_B, DenominatorOne) {
     EXPECT_EQ(INT_Q_B(a), 1);                 // 1 - число целое
 }
 
-TEST(test_INT_Q_B, DenominatorZero) {
-    LongInteger numerator(false, {1, 2, 3});               // 123
-    LongNatural denominator({0});                          // 0
-    LongRational a(numerator, denominator);                // 123 / 0
-    EXPECT_THROW({ INT_Q_B(a); }, std::invalid_argument);  // проверка на выброс ошибки
-}
-
 TEST(test_INT_Q_B, DenominatorNotOne) {
     LongInteger numerator(false, {1, 2, 3});  // 123
     LongNatural denominator({2});             // 2
