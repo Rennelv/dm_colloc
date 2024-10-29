84e872ae54978ab436225e49a7435b648eb6aef4
aassiya <aksreeva@mail.ru>
2024-10-29 13:47:36 +0300

    errors translated into english

--- src/Z/TRANS_Z_N.cpp
+++ src/Z/TRANS_Z_N.cpp
@@ -5,7 +5,7 @@
 LongNatural TRANS_Z_N(const LongInteger &n) {
     std::vector<uint8_t> newArr = n.getArr();  // получаем массив цифр целого числа n
     if (newArr[0] == 0) {
-        throw std::logic_error("Ошибка: число должно быть положительным");  // если n = 0, выбрасываем ошибку
+        throw std::logic_error("Error: number must be positive");  // если n = 0, выбрасываем ошибку
     } else {
         LongNatural result(newArr);  // создаем натуральное число
         return result;               // возвращаем результат
