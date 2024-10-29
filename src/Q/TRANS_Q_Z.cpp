c9c2f64f4f5e0e703b78dda34f1afffe573bf525
aassiya <aksreeva@mail.ru>
2024-10-29 14:03:34 +0300

    errors translated into english, changed getting of numerator

--- src/Q/TRANS_Q_Z.cpp
+++ src/Q/TRANS_Q_Z.cpp
@@ -3,16 +3,15 @@
 #include <stdexcept>
 
 LongInteger TRANS_Q_Z(const LongRational &n) {
-    LongInteger numerator = n.getNumerator();            // получаем числитель
     LongNatural denominator = n.getDenominator();        // получаем знаменатель
     std::vector<uint8_t> newArr = denominator.getArr();  // получаем массив цифр знаменателя
     if (newArr[0] == 0) {
-        throw std::logic_error("Ошибка: знаменатель не может быть равен нулю");  // если знаменатель равен нулю, то выбрасываем ошибку
+        throw std::logic_error("Error: denominator cannot be zero");  // если знаменатель равен нулю, то выбрасываем ошибку
     }
     if (newArr[0] == 1 and denominator.getLength() == 1) {
         LongInteger numerator = n.getNumerator();  // получаем числитель
         return numerator;                          // возвращаем числитель
     } else {
-        throw std::logic_error("Ошибка: знаменатель не равен единице");  // знаменатель не равен единице
+        throw std::logic_error("Error: denominator is not equal to one");  // знаменатель не равен единице
     }
 }
