9d54bd3a78040b4ddb1c6e165114baf687783f01
qudsol <saldindaniil.com>
2024-10-29 16:30:15 +0300

    rename branch

--- tests/P/test_MUL_Pxk_P.cpp
+++ tests/P/test_MUL_Pxk_P.cpp
@@ -10,11 +10,10 @@ TEST(test_MUL_Pxk_P, NormalPolynomial) {
     LongRational num4 = LongRational(LongInteger("4"), LongNatural("1"));
     LongRational zero = LongRational(LongInteger("0"), LongNatural("1"));  // ноль для результата
     Polynomial a({num1, num2, num3, num4});
-    Polynomial res = MUL_Pxk_P(a, static_cast<size_t>(4)); 
-    Polynomial expect({num1, num2, num3, num4, zero, zero, zero, zero});
-    EXPECT_EQ(res, expect);  // ((8/3)x^3 + 3x^2 + 6x^1 + 4x^0) * x^4 = (8/3)x^7 + 3x^6 + 6x^5 + 4x^4
+    Polynomial res({num1, num2, num3, num4, zero, zero, zero, zero});
+    EXPECT_EQ(MUL_Pxk_P(a, static_cast<size_t>(4)), res);  // ((8/3)x^3 + 3x^2 + 6x^1 + 4x^0) * x^4 = (8/3)x^7 + 3x^6 + 6x^5 + 4x^4
 }
-/*
+
 TEST(test_MUL_Pxk_P, ZeroDegree) {
     LongRational num1 = LongRational(LongInteger("8"), LongNatural("3"));
     LongRational num2 = LongRational(LongInteger("3"), LongNatural("1"));
@@ -33,4 +32,4 @@ TEST(test_MUL_Pxk_P, FirstDegree) {
     Polynomial a({num1, num2, num3, num4});
     Polynomial res({num1, num2, num3, num4, zero});
     EXPECT_EQ(MUL_Pxk_P(a, static_cast<size_t>(1)), res);  // ((8/3)x^3 + 3x^2 + 6x^1 + 4x^0) * x^1 = (8/3)x^4 + 3x^3 + 6x^2 + 4x^1
-}*/
+}
