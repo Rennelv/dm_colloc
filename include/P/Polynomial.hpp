8b6d0c4f77a88a5c836722177f28b11783673973
rennelv <55945621+Rennelv@users.noreply.github.com>
2024-10-29 21:03:18 +0300

    wip 1

--- include/P/Polynomial.hpp
+++ include/P/Polynomial.hpp
@@ -1,9 +1,9 @@
 #ifndef POLYNOMIAL_HPP
 #define POLYNOMIAL_HPP
 
-#include <initializer_list>
-#include <vector>
+#include <map>
 
+#include "N/LongNatural.hpp"
 #include "Q/LongRational.hpp"
 /*
     Выполнил Журавлев Дмитрий 3381
@@ -13,21 +13,14 @@
 */
 
 class Polynomial {
-    std::vector<LongRational> coefficients;  // массив коэффициентов
+    std::map<LongNatural, LongRational> coefficients;  // словарь (мапа) коэффициентов (степень -> коэффициент)
+    LongNatural degree;                                // степень полинома
    public:
-    Polynomial(std::initializer_list<LongRational> list);  // создает полином из списка коэффициентов
-    Polynomial(size_t n, LongRational* arr);               // создает полином из массива коэффициентов
-    Polynomial(const std::vector<LongRational>& vec);      // создает полином из вектора коэффициентов
+    Polynomial(const std::map<LongNatural, LongRational>& map);  // создает полином из мапа коэффициентов
 
-    const std::vector<LongRational>& getArr() const;  // возвращает вектор коэффициентов (для работы лучше использовать методы at и getCoef)
-
-    // возвращает i-ый коэффициент из массива коэффициентов (0 - старший коэффициент)
-    LongRational at(size_t i) const;
-    // возвращает i-ый коэффициент при степени deg (0 - свободный член; если deg больше степени полинома или при степени deg коэффициента нет, возвращает 0/1)
-    LongRational getCoef(size_t deg) const;
-
-    size_t getDegree() const;      // возвращает степень полинома
-    std::string toString() const;  // возвращает строку полинома
+    LongRational getCoef(const LongNatural& degree) const;  // возвращает коэффициент при степени
+    LongNatural getDegree() const;                          // возвращает степень полинома
+    std::string toString() const;                           // возвращает строку полинома
 
     bool operator==(const Polynomial& other) const;  // оператор сравнения (проверяет равенство коэффициентов)
     bool operator!=(const Polynomial& other) const;  // оператор сравнения (проверяет равенство коэффициентов)
