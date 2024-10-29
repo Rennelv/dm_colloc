802bb36074376b9d4e1f2f5a27baa0ef7751d42a
kzkken <zhabiykzkken@mail.ru>
2024-10-29 14:38:32 +0300

    RED_Q_Q

--- include/Q/RED_Q_Q.hpp
+++ include/Q/RED_Q_Q.hpp
@@ -1,16 +1,16 @@
 #ifndef RED_Q_Q_HPP
 #define RED_Q_Q_HPP
 
-#include "../include/Q/LongRational.hpp"
+#include "Q/LongRational.hpp"
 
 /*
     Выполинала Уфимцева Алиса 3381
 
-    Функция RED_Q_Q принимает дробь и сокращает её (или выбрасывает ошибку, если знаменатель равен нулю).
+    Функция RED_Q_Q принимает дробь и сокращает её.
     Принимает:
         a - дробь
     Возвращает:
-        LongRational(p, q) - сокращенная дробь с числителем p, знаменателем q
+        LongRational - сокращенная дробь с числителем p, знаменателем q
 */
 
 LongRational RED_Q_Q(const LongRational &a);
--- src/Q/RED_Q_Q.cpp
+++ src/Q/RED_Q_Q.cpp
@@ -1,23 +1,20 @@
-#include "../include/Q/RED_Q_Q.hpp"
+#include "Q/RED_Q_Q.hpp"
 
 #include <iostream>
 #include <vector>
 
-#include "../include/N/LongNatural.hpp"
-#include "../include/Z/LongInteger.hpp"
-#include "../include/Q/LongRational.hpp"
-#include "../include/Z/ABS_Z_N.hpp"
-#include "../include/N/GCF_NN_N.hpp"
-#include "../include/Z/ABS_Z_N.hpp"
+#include "N/GCF_NN_N.hpp"
+#include "N/LongNatural.hpp"
+#include "Q/LongRational.hpp"
+#include "Z/ABS_Z_N.hpp"
+#include "Z/LongInteger.hpp"
 
 using namespace std;
 
 LongRational RED_Q_Q(const LongRational &a) {
-    if (!NZER_N_B(getDenominator())) {  // проверяем, что знаменатель не равен нулю
-        throw logic_error("The denominator cannot be zero");  // выбрасываем ошибку, если знаменатель равен нулю
-    }
     uint8_t gcf = GCF_NN_N(ABS_Z_N(getNumerator()), getDenominator());  // находим НОД числителя и знаменателя
-    LongInteger p = ABS_Z_N(getNumerator(), gcf);  // делим числитель на НОД
-    LongNatural q = ABS_Z_N(getDenominator(), gcf);  // делим знаменатель на НОД
-    return LongRational(p, q);  // возвращаем сокращенную дробь
+    LongInteger p = ABS_Z_N(getNumerator(), gcf);                       // делим числитель на НОД
+    LongNatural q = ABS_Z_N(getDenominator(), gcf);                     // делим знаменатель на НОД
+    LongRational b(p, q);                                               // создаем сокращенную дробь
+    return b;                                                           // возвращаем сокращенную дробь
 }
