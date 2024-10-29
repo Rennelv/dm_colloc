8cc6ab22bb8a6a08b0d120ed3e392cc5802749e0
aassiya <144445785+aassiya@users.noreply.github.com>
2024-10-30 00:16:21 +0300

    function TRANS_Z_N (#28)
    * first
    
    * second
    
    * imoirt added
    
    * import added
    
    * indent added
    
    * errors translated into english
    
    * null check and tests changed

---
+++ include/Z/TRANS_Z_N.hpp
@@ -0,0 +1,20 @@
+#ifndef TRANS_Z_N_HPP
+#define TRANS_Z_N_HPP
+
+#include "N/LongNatural.hpp"
+#include "Z/LongInteger.hpp"
+
+/*
+    Выполнила Киреева Асия 3381
+
+    Функция TRANS_Z_N преобразовывает целое неорицательное число в натуральное
+    Принимает:
+        n - целое неорицательное число
+    Возвращает:
+        если n >= 0 - возвращаем натуральное число
+        если n < 0 - ошибка
+*/
+
+LongNatural TRANS_Z_N(const LongInteger &n);
+
+#endif
