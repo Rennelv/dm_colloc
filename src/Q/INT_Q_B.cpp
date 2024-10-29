4924fd860d76ca71af7c86a50faf36353e542dc0
aassiya <aksreeva@mail.ru>
2024-10-29 13:52:37 +0300

    errors translated into english, interaction with numerator removed

--- src/Q/INT_Q_B.cpp
+++ src/Q/INT_Q_B.cpp
@@ -3,11 +3,10 @@
 #include <stdexcept>
 
 int INT_Q_B(const LongRational &n) {
-    LongInteger numerator = n.getNumerator();            // получаем числитель
     LongNatural denominator = n.getDenominator();        // получаем знаменатель
     std::vector<uint8_t> newArr = denominator.getArr();  // получаем массив цифр знаменателя
     if (newArr[0] == 0) {
-        throw std::logic_error("Ошибка: знаменатель не может быть равен нулю");  // если знаменатель равен нулю, то выбрасываем ошибку
+        throw std::logic_error("Error: denominator cannot be zero");  // если знаменатель равен нулю, то выбрасываем ошибку
     }
     if (newArr[0] == 1 and denominator.getLength() == 1) {
         return 1;  // если знаменатель равен единице, то число целое
