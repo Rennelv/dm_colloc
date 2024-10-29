bc08616b5a47dc4a47a4407c8ef13951c3d015d1
rennelv <55945621+Rennelv@users.noreply.github.com>
2024-10-29 15:16:35 +0300

    cli v0.23

--- src/cli/InputHandler.cpp
+++ src/cli/InputHandler.cpp
@@ -4,6 +4,7 @@
 #include <limits>
 
 #include "N/LongNatural.hpp"
+#include "Q/LongRational.hpp"
 #include "Z/LongInteger.hpp"
 
 LongNatural InputHandler::scanLongNatural() {
@@ -25,10 +26,9 @@ LongInteger InputHandler::scanLongInteger() {
 Polynomial InputHandler::scanPolynomial() {
     std::cout << "Задайте многочлен в виде пар чисел (степень, коэффициент) в порядке убывания степеней, ввод заканчивается после ввода коэффициента при "
                  "нулевой степени.\n";
-    std::cout << "Введите пару чисел (степень, коэффициент): ";
+    std::cout << "Введите степень: ";
     size_t degree;
-    std::string coeff;
-    std::cin >> degree >> coeff;
+    std::cin >> degree;
     if (std::cin.fail()) {
         std::cout << "Неверный ввод\n";
         std::cin.clear();
@@ -36,20 +36,20 @@ Polynomial InputHandler::scanPolynomial() {
         throw std::invalid_argument("Invalid input");
     }
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    LongInteger coefficient = LongInteger(coeff);
-    std::vector<LongInteger> coefficients(degree + 1, LongInteger("0"));
+    LongRational coefficient = scanLongRational();
+    std::vector<LongRational> coefficients(degree + 1, LongRational(LongInteger("0"), LongNatural("1")));
     size_t idx = coefficients.size() - 1 - degree;
     coefficients[idx] = coefficient;
     size_t last_degree = degree;
     while (degree != 0) {
-        std::cout << "Введите пару чисел (степень, коэффициент): ";
-        std::cin >> degree >> coeff;
+        std::cout << "Введите степень: ";
+        std::cin >> degree;
         if (degree >= last_degree) {
             std::cout << "Степень должна быть меньше предыдущей.\n";
             throw std::invalid_argument("Invalid degree");
         }
         idx = coefficients.size() - 1 - degree;
-        LongInteger coefficient = LongInteger(coeff);
+        LongRational coefficient = scanLongRational();
         coefficients[idx] = coefficient;
     }
     return Polynomial(coefficients);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
+#include <iostream>
+
+#include "P/Polynomial.hpp"
 #include "cli/Cli.hpp"
+#include "cli/InputHandler.hpp"
 
 int main() {
     // ЪЪЪ
 
     Cli cli;
     cli.run();
+
+    InputHandler inputHandler;
+    Polynomial p = inputHandler.scanPolynomial();
+
+    std::cout << p.toString() << std::endl;
 
     return 0;
 }
