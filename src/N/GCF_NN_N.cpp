cd0d9b0e9c0ebf43debabe94131797ae7d63b55f
LordPumpk1n <144445621+LordPumpk1n@users.noreply.github.com>
2024-10-30 19:51:42 +0300

    function GCF_NN_N (#48)
    * function GCF_NN_N
    
    * 1
    
    * 1
    
    * 111
    
    * 111

---
+++ src/N/GCF_NN_N.cpp
@@ -0,0 +1,23 @@
+#include "N/GCF_NN_N.hpp"
+
+#include "N/COM_NN_D.hpp"
+#include "N/MOD_NN_N.hpp"
+#include "N/NZER_N_B.hpp"
+
+LongNatural GCF_NN_N(const LongNatural& a, const LongNatural& b) {
+    LongNatural a_copy = a;  // копися числа а
+    LongNatural b_copy = b;  // копия числа b
+    LongNatural tmp({0});
+
+    /*  Алгоритм Евклида(модифицированный)
+        Заменяет a на b, а b заменяет на остаток от деления a на b
+        до тех пор, пока b не станет нулём
+    */
+    while (NZER_N_B(b_copy)) {  // выполняется пока меньшее из чисел не равно нулю
+        tmp = b_copy;
+        b_copy = MOD_NN_N(a_copy, b_copy);  // деление с остатком
+        a_copy = tmp;
+    }
+
+    return a_copy;
+}
