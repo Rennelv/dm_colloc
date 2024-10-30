86a4f3b9a6f47973698ad660f2d5bf90179d93de
qudsol <saldindaniil.com>
2024-10-31 00:38:25 +0300

    MUL_Pxk_P

--- src/P/MUL_Pxk_P.cpp
+++ src/P/MUL_Pxk_P.cpp
@@ -3,16 +3,16 @@
 #include "N/ADD_NN_N.hpp"
 
 Polynomial MUL_Pxk_P(const Polynomial& a, const LongNatural& k) {
-    const std::map<LongNatural, LongRational>& PolyMap = a.getMap();  // возвращает вектор коэффициентов
-    std::map<LongNatural, LongRational> NewPolyMap;                   // Новый map для хранения результата
+    const std::map<LongNatural, LongRational>& poly_map = a.getMap();  // возвращает вектор коэффициентов
+    std::map<LongNatural, LongRational> new_poly_map;                  // Новый map для хранения результата
 
     // Проходим по всем элементам исходного polyMap
-    for (const auto& [key, value] : PolyMap) {
-        LongNatural NewKey = ADD_NN_N(key, k);  // Новый ключ
-        LongRational val = value;               // Значение остается прежним
-        NewPolyMap.emplace(NewKey, val);        // Используем emplace для добавления
+    for (const auto& [key, value] : poly_map) {
+        LongNatural new_key = ADD_NN_N(key, k);  // Новый ключ
+        LongRational val = value;                // Значение остается прежним
+        new_poly_map.emplace(new_key, val);      // Используем emplace для добавления
     }
 
     // Возвращаем новый полином с обновленным map
-    return Polynomial(NewPolyMap);
+    return Polynomial(new_poly_map);
 }
--- tests/P/test_MUL_Pxk_P.cpp
+++ tests/P/test_MUL_Pxk_P.cpp
@@ -5,55 +5,55 @@
 #include "P/Polynomial.hpp"
 
 TEST(test_MUL_Pxk_P, NormalPolynomial) {
-    std::map<LongNatural, LongRational> FstPol;
-    std::map<LongNatural, LongRational> ResPol;
+    std::map<LongNatural, LongRational> fst_pol;
+    std::map<LongNatural, LongRational> res_pol;
     LongRational num1 = LongRational(LongInteger("8"), LongNatural("3"));
     LongRational num2 = LongRational(LongInteger("3"), LongNatural("1"));
     LongRational num3 = LongRational(LongInteger("6"), LongNatural("1"));
     LongRational num4 = LongRational(LongInteger("4"), LongNatural("1"));
-    FstPol.emplace(LongNatural("3"), num1);
-    FstPol.emplace(LongNatural("2"), num2);
-    FstPol.emplace(LongNatural("1"), num3);
-    FstPol.emplace(LongNatural("0"), num4);
-    ResPol.emplace(LongNatural("7"), num1);
-    ResPol.emplace(LongNatural("6"), num2);
-    ResPol.emplace(LongNatural("5"), num3);
-    ResPol.emplace(LongNatural("4"), num4);
-    Polynomial a(FstPol);
-    Polynomial res(ResPol);
+    fst_pol.emplace(LongNatural("3"), num1);
+    fst_pol.emplace(LongNatural("2"), num2);
+    fst_pol.emplace(LongNatural("1"), num3);
+    fst_pol.emplace(LongNatural("0"), num4);
+    res_pol.emplace(LongNatural("7"), num1);
+    res_pol.emplace(LongNatural("6"), num2);
+    res_pol.emplace(LongNatural("5"), num3);
+    res_pol.emplace(LongNatural("4"), num4);
+    Polynomial a(fst_pol);
+    Polynomial res(res_pol);
     EXPECT_EQ(MUL_Pxk_P(a, LongNatural("4")), res);  // ((8/3)x^3 + 3x^2 + 6x^1 + 4x^0) * x^4 = (8/3)x^7 + 3x^6 + 6x^5 + 4x^4
 }
 
 TEST(test_MUL_Pxk_P, ZeroDegree) {
-    std::map<LongNatural, LongRational> FstPol;
+    std::map<LongNatural, LongRational> fst_pol;
     LongRational num1 = LongRational(LongInteger("8"), LongNatural("3"));
     LongRational num2 = LongRational(LongInteger("3"), LongNatural("1"));
     LongRational num3 = LongRational(LongInteger("6"), LongNatural("1"));
     LongRational num4 = LongRational(LongInteger("4"), LongNatural("1"));
-    FstPol.emplace(LongNatural("3"), num1);
-    FstPol.emplace(LongNatural("2"), num2);
-    FstPol.emplace(LongNatural("1"), num3);
-    FstPol.emplace(LongNatural("0"), num4);
-    Polynomial a(FstPol);
+    fst_pol.emplace(LongNatural("3"), num1);
+    fst_pol.emplace(LongNatural("2"), num2);
+    fst_pol.emplace(LongNatural("1"), num3);
+    fst_pol.emplace(LongNatural("0"), num4);
+    Polynomial a(fst_pol);
     EXPECT_EQ(MUL_Pxk_P(a, LongNatural("0")), a);  // ((8/3)x^3 + 3x^2 + 6x^1 + 4x^0) * x^0 = (8/3)x^3 + 3x^2 + 6x^1 + 4x^0
 }
 
 TEST(test_MUL_Pxk_P, FirstDegree) {
-    std::map<LongNatural, LongRational> FstPol;
-    std::map<LongNatural, LongRational> ResPol;
+    std::map<LongNatural, LongRational> fst_pol;
+    std::map<LongNatural, LongRational> res_pol;
     LongRational num1 = LongRational(LongInteger("8"), LongNatural("3"));
     LongRational num2 = LongRational(LongInteger("3"), LongNatural("1"));
     LongRational num3 = LongRational(LongInteger("6"), LongNatural("1"));
     LongRational num4 = LongRational(LongInteger("4"), LongNatural("1"));
-    FstPol.emplace(LongNatural("3"), num1);
-    FstPol.emplace(LongNatural("2"), num2);
-    FstPol.emplace(LongNatural("1"), num3);
-    FstPol.emplace(LongNatural("0"), num4);
-    ResPol.emplace(LongNatural("4"), num1);
-    ResPol.emplace(LongNatural("3"), num2);
-    ResPol.emplace(LongNatural("2"), num3);
-    ResPol.emplace(LongNatural("1"), num4);
-    Polynomial a(FstPol);
-    Polynomial res(ResPol);
+    fst_pol.emplace(LongNatural("3"), num1);
+    fst_pol.emplace(LongNatural("2"), num2);
+    fst_pol.emplace(LongNatural("1"), num3);
+    fst_pol.emplace(LongNatural("0"), num4);
+    res_pol.emplace(LongNatural("4"), num1);
+    res_pol.emplace(LongNatural("3"), num2);
+    res_pol.emplace(LongNatural("2"), num3);
+    res_pol.emplace(LongNatural("1"), num4);
+    Polynomial a(fst_pol);
+    Polynomial res(res_pol);
     EXPECT_EQ(MUL_Pxk_P(a, LongNatural("1")), res);  // ((8/3)x^3 + 3x^2 + 6x^1 + 4x^0) * x^1 = (8/3)x^4 + 3x^3 + 6x^2 + 4x^1
 }
