287138ae15c004479c524469c3d9d546672db60a
LordPumpk1n <gladilin20052@gmail.com>
2024-10-30 19:18:55 +0300

    1

--- src/N/GCF_NN_N.cpp
+++ src/N/GCF_NN_N.cpp
@@ -7,21 +7,14 @@
 LongNatural GCF_NN_N(const LongNatural& a, const LongNatural& b) {
     LongNatural a_copy = a;  // копися числа а
     LongNatural b_copy = b;  // копия числа b
+    LongNatural tmp({0});
 
-    // алгоритм Евклида
-    while (NZER_N_B(a_copy) && NZER_N_B(b_copy)) {  // если одно из числе станет равным нулю, то НОД найден
-
-        // Деление с остатком большего из двух чисел на меньшее
-        if (COM_NN_D(a_copy, b_copy) == 2) {
-            a_copy = MOD_NN_N(a_copy, b_copy);
-        } else {
-            b_copy = MOD_NN_N(b_copy, a_copy);
-        }
+    // алгоритм Евклида(модифицированный)
+    while (NZER_N_B(b_copy)) {  // выполняется пока меньшее из чисел не равно нулю
+        tmp = b_copy;
+        b_copy = MOD_NN_N(a_copy, b_copy);  // деление с остатком
+        a_copy = tmp;
     }
 
-    if (COM_NN_D(a_copy, b_copy) == 2) {  // возращает большее число, которое является НОД
-        return a_copy;
-    } else {
-        return b_copy;
-    }
+    return a_copy;
 }
