8154198945a4c7d2595ff778201525bf43c89e7b
kzkken <144446221+kzkken@users.noreply.github.com>
2024-10-30 21:19:18 +0300

    RED_Q_Q (#21)
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q
    
    * RED_Q_Q

---
+++ src/Q/RED_Q_Q.cpp
@@ -0,0 +1,20 @@
+#include "Q/RED_Q_Q.hpp"
+
+#include "N/DIV_NN_N.hpp"
+#include "N/GCF_NN_N.hpp"
+#include "N/LongNatural.hpp"
+#include "Q/LongRational.hpp"
+#include "Z/ABS_Z_N.hpp"
+#include "Z/DIV_ZZ_Z.hpp"
+#include "Z/LongInteger.hpp"
+
+using namespace std;
+
+LongRational RED_Q_Q(const LongRational &a) {
+    LongNatural gcf = GCF_NN_N(ABS_Z_N(a.getNumerator()), a.getDenominator());  // находим НОД числителя и знаменателя
+    LongInteger gcfInt(false, gcf.getArr());                                    // делаем НОД целым числом
+    LongInteger p = DIV_ZZ_Z(a.getNumerator(), gcfInt);                         // делим числитель на НОД (деленеие целых)
+    LongNatural q = DIV_NN_N(a.getDenominator(), gcf);  // делим знаменатель на НОД (деление натуральных)
+    LongRational b(p, q);                               // создаем сокращенную дробь
+    return b;                                           // возвращаем сокращенную дробь
+}
---
+++ tests/Q/test_RED_Q_Q.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+
+#include "N/LongNatural.hpp"
+#include "Q/LongRational.hpp"
+#include "Q/RED_Q_Q.hpp"
+#include "Z/LongInteger.hpp"
+
+using namespace std;
+
+TEST(Test_RED_Q_Q, Q1) {
+    LongInteger p1(false, {3});
+    LongNatural q1({6});
+    LongRational a(p1, q1);
+
+    LongInteger p2(false, {1});
+    LongNatural q2({2});
+    LongRational b(p2, q2);
+
+    LongInteger p3(false, {1, 2});
+    LongNatural q3({2, 4});
+    LongRational c(p3, q3);
+
+    LongInteger p4(true, {1, 2});
+    LongNatural q4({2, 4});
+    LongRational d(p4, q4);
+
+    LongInteger p2_neg(true, {1});
+    LongRational e(p2_neg, q2);
+
+    LongInteger null(false, {0});
+    LongNatural q5({1});
+    LongRational f(null, q2);
+    LongRational g(null, q5);
+
+    EXPECT_EQ(RED_Q_Q(a), b);
+    EXPECT_EQ(RED_Q_Q(b), b);
+    EXPECT_EQ(RED_Q_Q(c), b);
+    EXPECT_EQ(RED_Q_Q(d), e);
+    EXPECT_EQ(RED_Q_Q(f), g);
+}
