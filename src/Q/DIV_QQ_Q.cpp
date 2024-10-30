1cba02d16d5f3ba409a3788f5fc2ce1f35942c2d
kzkken <zhabiykzkken@mail.ru>
2024-10-30 22:25:19 +0300

    DIV_QQ_Q

--- src/Q/DIV_QQ_Q.cpp
+++ src/Q/DIV_QQ_Q.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-LongRational MUL_QQ_Q(const LongRational &a, const LongRational &b) {
+LongRational DIV_QQ_Q(const LongRational &a, const LongRational &b) {
     LongInteger p(b.getNumerator().isNegative(),
                   b.getDenominator().getArr());  // создаем целое число p по модулю равное знаменателю второй дроби и знака второй дроби
     LongNatural q(b.getNumerator().getArr());  // создаем натуральное число q равное по модулю числителю второй дроби
