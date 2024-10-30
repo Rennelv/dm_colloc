082591c569b113a97ae64543b5ee7d2a4bba2d8c
aassiya <144445785+aassiya@users.noreply.github.com>
2024-10-30 23:20:02 +0300

    function SUB_ZZ_Z (#53)
    * first
    
    * second
    
    * third
    
    * condition changed

---
+++ include/Z/SUB_ZZ_Z.hpp
@@ -0,0 +1,20 @@
+#ifndef SUB_ZZ_Z_HPP
+#define SUB_ZZ_Z_HPP
+
+#include "N/LongNatural.hpp"
+#include "Z/LongInteger.hpp"
+
+/*
+    Выполнила Киреева Асия 3381
+
+    Функция SUB_ZZ_Z выполняет вычитание двух целых чисел
+    Принимает:
+        a - целое число
+        b - целое число
+    Возвращает:
+        резльтат вычитания второго числа из первого
+*/
+
+LongInteger SUB_ZZ_Z(const LongInteger &a, const LongInteger &b);
+
+#endif
