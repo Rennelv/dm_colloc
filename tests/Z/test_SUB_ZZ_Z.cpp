774394e8f94575c88a63dcb9f08309f65f346358
aassiya <aksreeva@mail.ru>
2024-10-30 21:53:59 +0300

    second

--- tests/Z/test_SUB_ZZ_Z.cpp
+++ tests/Z/test_SUB_ZZ_Z.cpp
@@ -6,41 +6,41 @@
 TEST(test_SUB_ZZ_Z, PositiveNumbersFirstGreater) {
     LongInteger a(false, {1, 2, 3});          // 123
     LongInteger b(false, {5, 6});             // 56
-    EXPECT_EQ(SUB_ZZ_Z(a).toString(), "67");  // 123 - 56 = 67
+    EXPECT_EQ(SUB_ZZ_Z(a, b).toString(), "67");  // 123 - 56 = 67
 }
 
 TEST(test_SUB_ZZ_Z, PositiveNumbersSecondGreater) {
     LongInteger a(false, {5, 6});              // 56
     LongInteger b(false, {1, 2, 3});           // 123
-    EXPECT_EQ(SUB_ZZ_Z(a).toString(), "-67");  // 56 - 123 = -67
+    EXPECT_EQ(SUB_ZZ_Z(a, b).toString(), "-67");  // 56 - 123 = -67
 }
 
 TEST(test_SUB_ZZ_Z, NegativeNumbersFirstGreater) {
     LongInteger a(true, {1, 2, 3});            // -123
     LongInteger b(true, {5, 6});               // -56
-    EXPECT_EQ(SUB_ZZ_Z(a).toString(), "-67");  // -123 + 56 = -67
+    EXPECT_EQ(SUB_ZZ_Z(a, b).toString(), "-67");  // -123 + 56 = -67
 }
 
 TEST(test_SUB_ZZ_Z, NegativeNumbersSecondGreater) {
     LongInteger a(true, {5, 6});              //-56
     LongInteger b(true, {1, 2, 3});           //-123
-    EXPECT_EQ(SUB_ZZ_Z(a).toString(), "67");  // -56 + 123 = 67
+    EXPECT_EQ(SUB_ZZ_Z(a, b).toString(), "67");  // -56 + 123 = 67
 }
 
 TEST(test_SUB_ZZ_Z, PositiveAndNegative) {
     LongInteger a(false, {1, 2, 3});           // 123
     LongInteger b(true, {5, 6});               // -56
-    EXPECT_EQ(SUB_ZZ_Z(a).toString(), "179");  // 123 + 56 = 179
+    EXPECT_EQ(SUB_ZZ_Z(a, b).toString(), "179");  // 123 + 56 = 179
 }
 
 TEST(test_SUB_ZZ_Z, NegativeAndPositive) {
     LongInteger a(true, {1, 2, 3});             // -123
     LongInteger b(false, {5, 6});               // 56
-    EXPECT_EQ(SUB_ZZ_Z(a).toString(), "-179");  // -123 -56 = -179
+    EXPECT_EQ(SUB_ZZ_Z(a, b).toString(), "-179");  // -123 -56 = -179
 }
 
 TEST(test_SUB_ZZ_Z, NumbersAreEquals) {
     LongInteger a(true, {1, 2, 3});          // 123
     LongInteger b(true, {1, 2, 3});          // 123
-    EXPECT_EQ(SUB_ZZ_Z(a).toString(), "0");  // 123 - 123 = 0
+    EXPECT_EQ(SUB_ZZ_Z(a, b).toString(), "0");  // 123 - 123 = 0
 }
