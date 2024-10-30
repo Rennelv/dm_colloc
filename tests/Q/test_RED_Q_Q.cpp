dd891989472fecf5af17a3a89f21e76d3b2c1969
kzkken <zhabiykzkken@mail.ru>
2024-10-30 20:23:47 +0300

    RED_Q_Q

--- tests/Q/test_RED_Q_Q.cpp
+++ tests/Q/test_RED_Q_Q.cpp
@@ -1,10 +1,11 @@
 #include <gtest/gtest.h>
+
 #include <vector>
 
 #include "N/LongNatural.hpp"
-#include "Z/LongInteger.hpp"
 #include "Q/LongRational.hpp"
 #include "Q/RED_Q_Q.hpp"
+#include "Z/LongInteger.hpp"
 
 using namespace std;
 
@@ -26,8 +27,8 @@ TEST(Test_RED_Q_Q, Q1) {
     LongRational d(p4, q4);
 
     LongInteger p2_neg(true, {1});
-    LongRational e(p4_neg, q2);
-    
+    LongRational e(p2_neg, q2);
+
     EXPECT_EQ(RED_Q_Q(a), b);
     EXPECT_EQ(RED_Q_Q(b), b);
     EXPECT_EQ(RED_Q_Q(c), b);
