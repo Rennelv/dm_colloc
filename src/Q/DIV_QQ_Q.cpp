5916ecada2dd00182c2d26a31f320ba929fcc416
kzkken <zhabiykzkken@mail.ru>
2024-10-30 22:08:53 +0300

    DIV_QQ_Q

--- src/Q/DIV_QQ_Q.cpp
+++ src/Q/DIV_QQ_Q.cpp
@@ -10,7 +10,7 @@ LongRational MUL_QQ_Q(const LongRational &a, const LongRational &b) {
     LongInteger p(b.getNumerator().isNegative(),
                   b.getDenominator().getArr());  // создаем целое число p по модулю равное знаменателю второй дроби и знака второй дроби
     LongNatural q(b.getNumerator().getArr());  // создаем натуральное число q равное по модулю числителю второй дроби
-    LongRational с(p, q);  // создаем дробь с числителем p и знаменателем q ("перевернутая" вторая дробь)
+    LongRational c(p, q);  // создаем дробь с числителем p и знаменателем q ("перевернутая" вторая дробь)
     LongRational ans = MUL_QQ_Q(a, c);  // перемножаем первую дробь и "перевернутую" вторую дробь
     ans = RED_Q_Q(ans);                 // сокращаем полученную дробь
     return ans;                         // возвращаем сокращенную дробь
