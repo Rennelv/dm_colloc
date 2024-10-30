02d0ed02e431119170110798a909710027f280fb
LordPumpk1n <gladilin20052@gmail.com>
2024-10-30 14:35:16 +0300

    111

--- src/N/MOD_NN_N.cpp
+++ src/N/MOD_NN_N.cpp
@@ -10,9 +10,9 @@ LongNatural MOD_NN_N(LongNatural& a, LongNatural& b) {
         throw std::logic_error("Divider is zero");
     }
     // халтурный момент, исправиттся сразу после фикса деления нацело
-    LongNatural a_copy = a;
-    LongNatural integerPart = DIV_NN_N(a_copy, b);  // Получаем целую часть от деления
-    LongNatural tmp = MUL_NN_N(b, integerPart);     // Умножаем целую часть от деления на делитель
+
+    LongNatural integerPart = DIV_NN_N(a, b);    // Получаем целую часть от деления
+    LongNatural tmp = MUL_NN_N(b, integerPart);  // Умножаем целую часть от деления на делитель
     LongNatural result = SUB_NN_N(a, tmp);  // Получаем остаток путём вычитания из делимого значения из прошлой строки
 
     return result;
