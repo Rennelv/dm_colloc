316fd4cfe1ce8b6e2730cb2ccb6155e5c1d14225
33sasha81 <sasaderevagina87@gmail.com>
2024-10-31 18:00:22 +0300

    function DER_P_P

--- src/P/DER_P_P.cpp
+++ src/P/DER_P_P.cpp
@@ -10,19 +10,18 @@
 #include <Z/TRANS_N_Z.hpp>
 
 Polynomial DER_P_P(const Polynomial& a) {
-    const LongNatural& aDeg = a.getDegree();  //получаем степень исходного полинома
-    LongNatural aDegree = ADD_1N_N(aDeg);  // увеличиваем значение степени на 1 (чтобы в цикле пройтись по всем степеням)
-    LongNatural one({1});                              //создаем натуральное число 1
-    std::map<LongNatural, LongRational> coefficients;  // создаем map для хранения результата
-    //проходим по всем степеням исходного полинома (в том числе с нулевыми коэффициентами)
-    for (LongNatural i{1}; COM_NN_D(i, aDegree) == 1; i = ADD_1N_N(i)) {
-        //проверяем чтобы коэффициент не был равен 0
-        if (!a.isCoefZero(i)) {
-            const LongRational& coeff = a.getCoef(i);            //получаем коэффициент полинома при данной степени
-            LongInteger deg = TRANS_N_Z(i);                      //преобразуем натуральное число в целое
+    const std::map<LongNatural, LongRational>& map = a.getMap();  // получаем map числа
+    LongNatural one({1});                                         //создаем натуральное число 1
+    LongNatural zero({0});                                        //создаем натуральное число 0
+    std::map<LongNatural, LongRational> coefficients;             // создаем map для хранения результата
+    // проходимся по степеням полинома, у которых не нулевые коэффициенты
+    for (const auto& [key, value] : map) {
+        // проверяем, что степень не равна 0
+        if (COM_NN_D(key, zero) != 0) {
+            LongInteger deg = TRANS_N_Z(key);                    //преобразуем натуральное число в целое
             LongRational degree(deg, one);                       //преобразуем целое число в рациональное
-            LongRational coefficient = MUL_QQ_Q(coeff, degree);  //перемножаем значение коэффициента и значение степени
-            coefficients.emplace(SUB_NN_N(i, one),
+            LongRational coefficient = MUL_QQ_Q(value, degree);  //перемножаем значение коэффициента и значение степени
+            coefficients.emplace(SUB_NN_N(key, one),
                                  coefficient);  //добавляем в map нового полинома значение полученного полинома по ключу исходной степени уменьшенной на 1
         }
     }
