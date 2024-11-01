ffd21f6b110dd9e91d9d25660e60801012740f0f
LordPumpk1n <144445621+LordPumpk1n@users.noreply.github.com>
2024-11-01 22:34:55 +0300

    function FAC_P_Q (#60)
    * function FAC_P_Q
    
    * 1

---
+++ src/P/FAC_P_Q.cpp
@@ -0,0 +1,46 @@
+#include "P/FAC_P_Q.hpp"
+
+#include "N/DIV_NN_N.hpp"
+#include "N/GCF_NN_N.hpp"
+#include "N/LCM_NN_N.hpp"
+#include "N/LongNatural.hpp"
+#include "Z/ABS_Z_N.hpp"
+#include "Z/DIV_ZZ_Z.hpp"
+#include "Z/LongInteger.hpp"
+#include "Z/MUL_ZZ_Z.hpp"
+#include "Z/TRANS_N_Z.hpp"
+
+LongRational FAC_P_Q(Polynomial& a) {
+    const std::map<LongNatural, LongRational>& map = a.getMap();  // возвращает мап коэффициентов
+    std::map<LongNatural, LongRational> new_map;                  // новый map для хранения результата
+
+    LongNatural gcf = {0};  // Для хранения НОД
+    LongNatural lcm = {1};  // Для хранения НОК
+    LongNatural tmp_numerator = {0};
+
+    // Проходим по всем коэффициентам многочлена не равным нулю
+    for (const auto& [key, value] : map) {
+        tmp_numerator = ABS_Z_N(value.getNumerator());  // Временная переменная для получения абсолютного значения числа
+        gcf = GCF_NN_N(gcf, tmp_numerator);           // Считаем НОД всех числителей
+        lcm = LCM_NN_N(lcm, value.getDenominator());  // Считаем НОК всех знаменателей
+    }
+
+    LongInteger gcf_long_integer = TRANS_N_Z(gcf);  // Преобразование натурального в целое число
+    LongNatural new_denominatior("1");  // Новый знаменатель будет равен 1, так как из многочлена выносится НОК всех знаменателей
+    LongInteger factor("0");  // Значение, на которое домножается числитель(НОК раздёленный на знаменатель)
+
+    for (const auto& [key, value] : map) {
+        LongInteger new_numerator = DIV_ZZ_Z(value.getNumerator(), gcf_long_integer);  // "Выносим" НОД из числителя
+        factor = TRANS_N_Z(DIV_NN_N(lcm, value.getDenominator()));                     // "Выносим" НОК из знаменателя
+        new_numerator = MUL_ZZ_Z(new_numerator, factor);                               // Получаем новый числитель
+        LongRational new_value = {new_numerator, new_denominatior};  // Новый коэффициент(числитель новый, а знаменатель равен 1)
+        new_map.emplace(key, new_value);                             // Помещаем новый коэффициент в мап
+    }
+
+    // Изменяем изначальный многочлен
+    Polynomial new_poly(new_map);
+    a = new_poly;
+
+    // Возвращаем вынесенный коффициент
+    return LongRational(gcf_long_integer, lcm);
+}
---
+++ tests/P/test_FAC_P_Q.cpp
@@ -0,0 +1,97 @@
+#include <gtest/gtest.h>
+
+#include "N/LongNatural.hpp"
+#include "P/FAC_P_Q.hpp"
+#include "P/Polynomial.hpp"
+#include "Q/LongRational.hpp"
+#include "Z/LongInteger.hpp"
+
+TEST(test_FAC_P_Q, IntegerCoefficients) {
+    std::map<LongNatural, LongRational> poly;
+    poly.emplace(LongNatural("2"), LongRational(LongInteger("6"), LongNatural("1")));
+    poly.emplace(LongNatural("1"), LongRational(LongInteger("9"), LongNatural("1")));
+    poly.emplace(LongNatural("0"), LongRational(LongInteger("12"), LongNatural("1")));
+    Polynomial a(poly);
+    LongRational result = FAC_P_Q(a);
+
+    std::map<LongNatural, LongRational> expect_poly;
+    expect_poly.emplace(LongNatural("2"), LongRational(LongInteger("2"), LongNatural("1")));
+    expect_poly.emplace(LongNatural("1"), LongRational(LongInteger("3"), LongNatural("1")));
+    expect_poly.emplace(LongNatural("0"), LongRational(LongInteger("4"), LongNatural("1")));
+    Polynomial expect(expect_poly);
+
+    EXPECT_EQ(a.toString(), expect.toString());          // (6/1)x^2 + (9/1)x^1 + (12/1)x^0 = [(2/1)x^2 + (3/1)x^1 + (4/1)x^0] * (3/1)
+    EXPECT_EQ(result.getNumerator().toString(), "3");    // Ожидаемый НОД = 3
+    EXPECT_EQ(result.getDenominator().toString(), "1");  // Ожидаемый НОК 1
+}
+
+TEST(test_FAC_P_Q, FractionalCoefficients) {
+    std::map<LongNatural, LongRational> poly;
+    poly.emplace(LongNatural("2"), LongRational(LongInteger("4"), LongNatural("5")));
+    poly.emplace(LongNatural("1"), LongRational(LongInteger("6"), LongNatural("10")));
+    poly.emplace(LongNatural("0"), LongRational(LongInteger("8"), LongNatural("15")));
+    Polynomial a(poly);
+    LongRational result = FAC_P_Q(a);
+
+    std::map<LongNatural, LongRational> expect_poly;
+    expect_poly.emplace(LongNatural("2"), LongRational(LongInteger("12"), LongNatural("1")));
+    expect_poly.emplace(LongNatural("1"), LongRational(LongInteger("9"), LongNatural("1")));
+    expect_poly.emplace(LongNatural("0"), LongRational(LongInteger("8"), LongNatural("1")));
+    Polynomial expect(expect_poly);
+
+    EXPECT_EQ(a.toString(), expect.toString());           // (4/5)x^2 + (6/10)x^1 + (8/15)x^0 = [(12/1)x^2 + (9/1)x^1 + (8/1)x^0] * (2/30)
+    EXPECT_EQ(result.getNumerator().toString(), "2");     // Ожидаемый НОД = 2
+    EXPECT_EQ(result.getDenominator().toString(), "30");  // Ожидаемый НОК = 30
+}
+
+TEST(test_FAC_P_Q, GCFAndLCMAreOne) {
+    std::map<LongNatural, LongRational> poly;
+    poly.emplace(LongNatural("1"), LongRational(LongInteger("7"), LongNatural("1")));
+    poly.emplace(LongNatural("0"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial a(poly);
+    LongRational result = FAC_P_Q(a);
+
+    std::map<LongNatural, LongRational> expect_poly;
+    expect_poly.emplace(LongNatural("1"), LongRational(LongInteger("7"), LongNatural("1")));
+    expect_poly.emplace(LongNatural("0"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial expect(expect_poly);
+
+    EXPECT_EQ(a.toString(), expect.toString());          // (7/1)x^1 + (1/1)x^0 = (7/1)x^1 + (1/1)x^0
+    EXPECT_EQ(result.getNumerator().toString(), "1");    // Ожидаемый НОД = 1
+    EXPECT_EQ(result.getDenominator().toString(), "1");  // Ожидаемый НОК  = 1
+}
+
+TEST(test_FAC_P_Q, ZeroCoefficient) {
+    std::map<LongNatural, LongRational> poly;
+    poly.emplace(LongNatural("3"), LongRational(LongInteger("12"), LongNatural("1")));
+    poly.emplace(LongNatural("2"), LongRational(LongInteger("0"), LongNatural("1")));
+    poly.emplace(LongNatural("1"), LongRational(LongInteger("18"), LongNatural("1")));
+    poly.emplace(LongNatural("0"), LongRational(LongInteger("24"), LongNatural("1")));
+    Polynomial a(poly);
+    LongRational result = FAC_P_Q(a);
+
+    std::map<LongNatural, LongRational> expect_poly;
+    expect_poly.emplace(LongNatural("3"), LongRational(LongInteger("2"), LongNatural("1")));
+    expect_poly.emplace(LongNatural("1"), LongRational(LongInteger("3"), LongNatural("1")));
+    expect_poly.emplace(LongNatural("0"), LongRational(LongInteger("4"), LongNatural("1")));
+    Polynomial expect(expect_poly);
+
+    EXPECT_EQ(a.toString(), expect.toString());          // (12/1)x^3 + (18/1)x^1 + (24/1)x^0 = [(2/1)x^3 + (3/1)x^1 + (4/1)x^0] * (6/1)
+    EXPECT_EQ(result.getNumerator().toString(), "6");    // Ожидаемый НОД = 6
+    EXPECT_EQ(result.getDenominator().toString(), "1");  // Ожидаемый НОК = 1
+}
+
+TEST(test_FAC_P_Q, ZeroPolynomial) {
+    std::map<LongNatural, LongRational> poly;
+    poly.emplace(LongNatural("0"), LongRational(LongInteger("0"), LongNatural("1")));
+    Polynomial a(poly);
+    LongRational result = FAC_P_Q(a);
+
+    std::map<LongNatural, LongRational> expect_poly;
+    expect_poly.emplace(LongNatural("0"), LongRational(LongInteger("0"), LongNatural("1")));
+    Polynomial expect(expect_poly);
+
+    EXPECT_EQ(a.toString(), expect.toString());          // (0/1)x^0 = (0/1)x^0
+    EXPECT_EQ(result.getNumerator().toString(), "0");    // Ожидаемый НОД = 0ы
+    EXPECT_EQ(result.getDenominator().toString(), "1");  // Ожидаемый НОК = 1
+}
