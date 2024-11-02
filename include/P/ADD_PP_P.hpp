d87780b4c5152f5c0c49d8097e003995c014162e
kunakkova <kunakovamaria@mail.ru>
2024-11-02 16:53:45 +0300

    function ADD_PP_P

---
+++ include/P/ADD_PP_P.hpp
@@ -0,0 +1,21 @@
+#ifndef ADD_PP_P_HPP
+#define ADD_PP_P_HPP
+
+#include "P/Polynomial.hpp"
+
+/*
+
+    Выполнила Кунакова Мария 3381
+
+    Функция ADD_PP_P складывает два многочлена.
+    Принимает:
+        a - первый многочлен
+        b - второй многочлен
+    Возвращает:
+        Polynomial - результат сложения
+
+*/
+
+Polynomial ADD_PP_P(const Polynomial& a, const Polynomial& b);
+
+#endif  // ADD_PP_P
---
+++ src/P/ADD_PP_P.cpp
@@ -0,0 +1,31 @@
+#include "P/ADD_PP_P.hpp"
+
+#include "N/LongNatural.hpp"
+#include "Q/ADD_QQ_Q.hpp"
+#include "Q/LongRational.hpp"
+
+Polynomial ADD_PP_P(const Polynomial& a, const Polynomial& b) {
+    std::map<LongNatural, LongRational> resultCoefficients = a.getMap();  // Копируем коэффициенты первого многочлена
+
+    for (const auto& [degree, coefB] : b.getMap()) {
+        auto it = resultCoefficients.find(degree);
+        if (it != resultCoefficients.end()) {
+            // Если такая степень уже есть в первом многочлене, складываем коэффициенты
+            it->second = ADD_QQ_Q(it->second, coefB);
+        } else {
+            // Если такая степень отсутствует, просто добавляем новый член с данным коэффициентом
+            resultCoefficients.emplace(degree, coefB);
+        }
+    }
+
+    // Убираем нулевые коэффициенты из результирующего многочлена
+    for (auto it = resultCoefficients.begin(); it != resultCoefficients.end();) {
+        if (it->second.getNumerator() == LongInteger(false, {0})) {
+            it = resultCoefficients.erase(it);
+        } else {
+            ++it;
+        }
+    }
+
+    return Polynomial(resultCoefficients);
+}
