72d6d62f82f317434f52ec81ea7dd0ebb010ad1a
33sasha81 <sasaderevagina87@gmail.com>
2024-11-03 00:03:49 +0300

    function SUB_QQ_Q

--- src/Q/SUB_QQ_Q.cpp
+++ src/Q/SUB_QQ_Q.cpp
@@ -8,12 +8,16 @@
 #include "Z/TRANS_N_Z.hpp"
 
 LongRational SUB_QQ_Q(const LongRational& a, const LongRational& b) {
-    const LongNatural& denominator_a = a.getDenominator();     // получаем знаменатель первого числа
-    const LongNatural& denominator_b = b.getDenominator();     // получаем знаменатель второго числа
+    const LongNatural& denominator_a = a.getDenominator();  // получаем знаменатель первого числа
+    const LongNatural& denominator_b = b.getDenominator();  // получаем знаменатель второго числа
+
     LongNatural lcm = LCM_NN_N(denominator_a, denominator_b);  // находим НОК двух чисел
+
     LongInteger multiplier_a = TRANS_N_Z(DIV_NN_N(lcm, denominator_a));  // находим множитель для первого числа, деля НОК на знаменатель первого числа
     LongInteger multiplier_b = TRANS_N_Z(DIV_NN_N(lcm, denominator_b));  // находим множитель для второго числа, деля НОК на знаменатель второг числа
+
     LongInteger numerator = SUB_ZZ_Z(MUL_ZZ_Z(a.getNumerator(), multiplier_a), MUL_ZZ_Z(b.getNumerator(), multiplier_b));  // находим числитель
+
     LongRational result(numerator, lcm);  // создаем дробь с полученным числителем и знаменателем
     result = RED_Q_Q(result);             // сокращаем дробь
     return result;
