7adb12539aa9ffb8a69567df2569916cf88fe9a9
mikhailshceglov <158569383+mikhailshceglov@users.noreply.github.com>
2024-11-04 01:05:55 +0300

    function/DIV_PP_P (#70)
    * DIV_PP_P
    
    * fixed clang-format
    
    * fix
    
    * fix tests
    
    * fixed polynomial 1
    
    * fixed clang :(

---
+++ src/P/DIV_PP_P.cpp
@@ -0,0 +1,54 @@
+#include "P/DIV_PP_P.hpp"
+
+#include "N/SUB_NN_N.hpp"
+#include "P/ADD_PP_P.hpp"
+#include "P/DEG_P_N.hpp"
+#include "P/MUL_PQ_P.hpp"
+#include "P/MUL_Pxk_P.hpp"
+#include "P/SUB_PP_P.hpp"
+#include "Q/DIV_QQ_Q.hpp"
+
+Polynomial DIV_PP_P(const Polynomial& dividend, const Polynomial& divisor) {
+    // Проверка на единичный многочлен (если divisor == 1, возвращаем dividend)
+    if (divisor.getDegree() == LongNatural("0") && divisor.getCoef(LongNatural("0")) == LongRational(LongInteger("1"), LongNatural("1"))) {
+        return dividend;
+    }
+
+    // Инициализируем нулевой многочлен для частного
+    std::map<LongNatural, LongRational> zero_map;
+    zero_map.emplace(LongNatural("0"), LongRational(LongInteger("0"), LongNatural("1")));
+    Polynomial quotient(zero_map);
+
+    // Степень делимого и делителя
+    LongNatural deg_dividend = DEG_P_N(dividend);
+    LongNatural deg_divisor = DEG_P_N(divisor);
+
+    Polynomial remainder = dividend;
+
+    // Пока степень остатка >= степени делителя
+    while (COM_NN_D(DEG_P_N(remainder), deg_divisor) != 1) {  // Используем функцию сравнения
+        // Вычисляем текущую степень частного как разность степеней
+        LongNatural degree_diff = SUB_NN_N(DEG_P_N(remainder), deg_divisor);  // Используем функцию вычитания
+
+        // Получаем коэффициенты старших членов
+        LongRational leading_coeff_dividend = remainder.getCoef(DEG_P_N(remainder));
+        LongRational leading_coeff_divisor = divisor.getCoef(deg_divisor);
+
+        // Делим коэффициенты
+        LongRational coeff_quotient = DIV_QQ_Q(leading_coeff_dividend, leading_coeff_divisor);
+
+        // Создаем одночлен с этим коэффициентом и нужной степенью
+        std::map<LongNatural, LongRational> single_term_map;
+        single_term_map.emplace(degree_diff, coeff_quotient);
+        Polynomial single_term(single_term_map);
+
+        // Добавляем одночлен к частному
+        quotient = ADD_PP_P(quotient, single_term);
+
+        // Вычитаем произведение (divisor * одночлен) из остатка
+        Polynomial product = MUL_Pxk_P(MUL_PQ_P(divisor, coeff_quotient), degree_diff);
+        remainder = SUB_PP_P(remainder, product);
+    }
+
+    return quotient;
+}
---
+++ tests/P/test_DIV_PP_P.cpp
@@ -0,0 +1,85 @@
+#include <gtest/gtest.h>
+
+#include "N/LongNatural.hpp"
+#include "P/DIV_PP_P.hpp"
+#include "P/Polynomial.hpp"
+#include "Q/LongRational.hpp"
+#include "Z/LongInteger.hpp"
+
+// Тест деления многочленов с нулевым остатком
+TEST(DIV_PP_P, DivisionWithoutRemainder) {
+    // Создаем многочлен p1 = x^2 - 1 и делитель p2 = x - 1
+    std::map<LongNatural, LongRational> p1_map;
+    p1_map.emplace(LongNatural("2"), LongRational(LongInteger("1"), LongNatural("1")));
+    p1_map.emplace(LongNatural("0"), LongRational(LongInteger("-1"), LongNatural("1")));
+    Polynomial p1(p1_map);
+
+    std::map<LongNatural, LongRational> p2_map;
+    p2_map.emplace(LongNatural("1"), LongRational(LongInteger("1"), LongNatural("1")));
+    p2_map.emplace(LongNatural("0"), LongRational(LongInteger("-1"), LongNatural("1")));
+    Polynomial p2(p2_map);
+
+    // Ожидаемое частное: x + 1
+    std::map<LongNatural, LongRational> expected_map;
+    expected_map.emplace(LongNatural("1"), LongRational(LongInteger("1"), LongNatural("1")));
+    expected_map.emplace(LongNatural("0"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial expected(expected_map);
+
+    EXPECT_EQ(DIV_PP_P(p1, p2), expected);
+}
+
+// Тест деления многочлена на единичный многочлен
+TEST(DIV_PP_P, DivisionByOne) {
+    // Создаем многочлен p1 = x^2 + x + 1 и делитель p2 = 1
+    std::map<LongNatural, LongRational> p1_map;
+    p1_map.emplace(LongNatural("2"), LongRational(LongInteger("1"), LongNatural("1")));
+    p1_map.emplace(LongNatural("1"), LongRational(LongInteger("1"), LongNatural("1")));
+    p1_map.emplace(LongNatural("0"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial p1(p1_map);
+
+    std::map<LongNatural, LongRational> p2_map;
+    p2_map.emplace(LongNatural("0"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial p2(p2_map);
+
+    // Ожидаемое частное: x^2 + x + 1
+    Polynomial expected(p1_map);
+
+    EXPECT_EQ(DIV_PP_P(p1, p2), expected);
+}
+
+// Тест деления многочлена на более высокий по степени
+TEST(DIV_PP_P, DivisionByHigherDegree) {
+    // Создаем многочлен p1 = x^2 + x + 1 и делитель p2 = x^3
+    std::map<LongNatural, LongRational> p1_map;
+    p1_map.emplace(LongNatural("2"), LongRational(LongInteger("1"), LongNatural("1")));
+    p1_map.emplace(LongNatural("1"), LongRational(LongInteger("1"), LongNatural("1")));
+    p1_map.emplace(LongNatural("0"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial p1(p1_map);
+
+    std::map<LongNatural, LongRational> p2_map;
+    p2_map.emplace(LongNatural("3"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial p2(p2_map);
+
+    // Ожидаемое частное - нулевой многочлен
+    std::map<LongNatural, LongRational> zero_map;
+    zero_map.emplace(LongNatural("0"), LongRational(LongInteger("0"), LongNatural("1")));
+    Polynomial expected(zero_map);
+
+    EXPECT_EQ(DIV_PP_P(p1, p2), expected);
+}
+
+// Тест деления нулевого многочлена на ненулевой
+TEST(DIV_PP_P, ZeroDividend) {
+    // Создаем нулевой многочлен p1 и ненулевой многочлен p2
+    std::map<LongNatural, LongRational> zero_map;
+    Polynomial p1(zero_map);
+
+    std::map<LongNatural, LongRational> p2_map;
+    p2_map.emplace(LongNatural("1"), LongRational(LongInteger("1"), LongNatural("1")));
+    Polynomial p2(p2_map);
+
+    // Ожидаемое частное - нулевой многочлен
+    Polynomial expected(zero_map);
+
+    EXPECT_EQ(DIV_PP_P(p1, p2), expected);
+}
