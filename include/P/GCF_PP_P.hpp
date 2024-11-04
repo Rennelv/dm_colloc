6c875bb5db6e03b1126759959a986134508e29f1
mikhailshceglov <158569383+mikhailshceglov@users.noreply.github.com>
2024-11-04 13:56:58 +0300

    function/GCF_PP_P (#73)
    * GCF_PP_P
    
    * fixed gcd_pp_p.cpp

---
+++ include/P/GCF_PP_P.hpp
@@ -0,0 +1,19 @@
+#ifndef GCF_PP_P_HPP
+#define GCF_PP_P_HPP
+
+#include "P/Polynomial.hpp"
+
+/*
+    Выполнил: Уфимцева Алиса, группа 3381
+
+    Функция GCF_PP_P находит наибольший общий делитель (НОД) двух многочленов.
+    Принимает:
+        p1 - первый многочлен
+        p2 - второй многочлен
+    Возвращает:
+        Polynomial - НОД многочленов p1 и p2
+*/
+
+Polynomial GCF_PP_P(const Polynomial& p1, const Polynomial& p2);
+
+#endif  // GCF_PP_P_HPP
