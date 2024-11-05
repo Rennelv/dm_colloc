d2ea258704b5c09c3be310698aa368deb7be8b37
kzkken <zhabiykzkken@mail.ru>
2024-11-06 00:20:57 +0300

    omg colloc is over?

--- src/P/NMR_P_P.cpp
+++ src/P/NMR_P_P.cpp
@@ -1,12 +1,21 @@
 #include "P/NMR_P_P.hpp"
 
+#include "N/LongNatural.hpp"
 #include "P/DER_P_P.hpp"
 #include "P/DIV_PP_P.hpp"
 #include "P/GCF_PP_P.hpp"
+#include "Q/LongRational.hpp"
+#include "Z/LongInteger.hpp"
 
 Polynomial NMR_P_P(const Polynomial& p) {
-    Polynomial derivative = DER_P_P(p);        // Счиатем производную многочлена
-    Polynomial gcd = GCF_PP_P(p, derivative);  // Cчитаем НОД исходного p(x) и его производной
+    if (DEG_P_N(p) == LongNatural("0") && p.getCoef(LongNatural("0")) != LongRational(LongInteger("0"), LongNatural("1"))) {
+        std::map<LongNatural, LongRational> one_map;
+        one_map.emplace(LongNatural("0"), LongRational(LongInteger("1"), LongNatural("1")));
+        return Polynomial(one_map);
+    }
+
+    Polynomial derivative = DER_P_P(p);        // Считаем производную многочлена
+    Polynomial gcd = GCF_PP_P(p, derivative);  // Считаем НОД исходного p(x) и его производной
     Polynomial result = DIV_PP_P(p, gcd);      // Делим p(x) на НОД
 
     return result;
