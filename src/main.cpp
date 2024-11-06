f918f61a38e05378f986f00f94a16e14b47bbbe5
rennelv <55945621+Rennelv@users.noreply.github.com>
2024-11-06 14:24:43 +0300

    fix m

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
-#ifdef ENABLE_GUI
 #include "gui/Gui.hpp"
-#else
-#include "cli/Cli.hpp"
-#endif
 
 int main() {
-#ifdef ENABLE_GUI
     Gui gui;
     gui.run();
-#else
-    Cli cli;
-    cli.run();
-#endif
 
     return 0;
 }
