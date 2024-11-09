f20bf08660cc8b0caead6590d55ac452ddd20c61
LordPumpk1n <gladilin20052@gmail.com>
2024-11-10 01:28:06 +0300

    sum

--- src/N/ADD_NN_N.cpp
+++ src/N/ADD_NN_N.cpp
@@ -16,12 +16,11 @@ LongNatural ADD_NN_N(const LongNatural& a, const LongNatural& b) {
         b_arr = a.getArr();
     }
 
-    std::vector<uint8_t> result_arr;       // массив для результата
-    result_arr.reserve(a_arr.size() + 1);  // резервируем место для результата
+    a_arr.reserve(a_arr.size() + 1);  // резервируем место для результата
 
-    uint8_t carry = 0;  // перенос
-    size_t a_len = a_arr.size();
-    size_t b_len = b_arr.size();
+    uint8_t carry = 0;            // перенос
+    size_t a_len = a_arr.size();  // длина числа a
+    size_t b_len = b_arr.size();  // длина числа b
 
     for (size_t i = 0; i < a_len; i++) {
         size_t a_idx = a_len - i - 1;        // индекс для числа а с конца
@@ -32,15 +31,13 @@ LongNatural ADD_NN_N(const LongNatural& a, const LongNatural& b) {
             sum += b_arr[b_idx];           // если есть соответсвующая цифра числа b, то прибавляем ее
         }
 
-        carry = sum / 10;                // вычисляем перенос
-        result_arr.push_back(sum % 10);  // добавляем остаток от деления на 10 в результат
+        carry = sum / 10;         // вычисляем перенос
+        a_arr[a_idx] = sum % 10;  // добавляем остаток от деления на 10 в результат
     }
 
     if (carry != 0) {
-        result_arr.push_back(carry);  // если есть перенос, то добавляем его в результат
+        a_arr.insert(a_arr.begin(), carry);  // если есть перенос, то добавляем его в новый разряд
     }
 
-    std::reverse(result_arr.begin(), result_arr.end());  // переворачиваем массив
-
-    return LongNatural(result_arr);  // возвращаем результат
+    return LongNatural(a_arr);  // возвращаем результат
 }
